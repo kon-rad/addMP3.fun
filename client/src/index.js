38306fabeda8d6b237a479b6b5b4e21571235751
konradgnat <konradmgnat@gmail.com>
2018-04-17 21:18:16 -0500

    add dependecies, change folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const mountApp = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  mountApp
+);
 registerServiceWorker();
+
+// For hot reloading of react components
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(
+      <AppContainer>
+        <App />
+      </AppContainer>,
+      mountApp
+    );
+  });
+}
