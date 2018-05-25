4c4b8142b908a69fcf2de217d9faca79b5a8a43a
konradgnat <konradmgnat@gmail.com>
2018-05-25 07:55:06 -0500

    download audio

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const youtubedl = require('youtube-dl');
-
+var	ffmpeg = require('fluent-ffmpeg');
 
 /**
  * Post audio
  * @param req
  * @param res
  * @returns void
+ *
+ * test url:
+ * https://www.youtube.com/watch?v=Lo3769VtgHM
  */
 export function postAudio(req, res) {
 
@@ -22,8 +25,31 @@ export function postAudio(req, res) {
     console.log('size: ' + info.size);
   });
 
-  res.header({
-    'Content-Disposition': 'attachment; filename="youtubeAudio.mp3"'
+  video.pipe(fs.createWriteStream('test.mp3'));
+  video.on('end', function() {
+    res.writeHead(200, {
+      'Content-Type': 'audio/mpeg'
+    });
+    video.pipe(res);
   });
-  video.pipe(res);
+
+
+
+  // video.on('end', function() {
+  //   res.writeHead(200, {
+  //     'Content-Type': 'audio/mpeg'
+  //   });
+  // })
+
+
+  // video.pipe(fs.createWriteStream('myvideo.mp3'));
+
+  // var proc = new ffmpeg({source:video});
+  // proc.setFfmpegPath('/Applications/ffmpeg/ffmpeg');
+  // proc.save(mp3 + '.mp3');
+
+  // res.header({
+  //   'Content-Disposition': 'attachment; filename="youtubeAudio.mp3"'
+  // });
+
 }
