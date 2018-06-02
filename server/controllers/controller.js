2bdf9a3f30ff60be25f20bc6288bb08efac4464c
konradgnat <konradmgnat@gmail.com>
2018-06-02 07:13:20 -0500

    delete file on download

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,7 +3,6 @@ let	ffmpeg = require('fluent-ffmpeg');
 let youtubedl = require('youtube-dl');
 let path = require('path');
 
-
 /**
  * Post audio
  * @param req
@@ -14,26 +13,24 @@ let path = require('path');
  * https://www.youtube.com/watch?v=Lo3769VtgHM
  */
 export function postAudio(req, res) {
-
   const url = req.body.post.url.trim();
-
   let filename = url.split('watch?v=')[1] + '.mp3';
-  // filename = filename.replace(/[ <>:"/\\|?*]/g, '_') + '.mp3';
-  console.log(`/tmp/${filename}`);
+  const file = path.join(__dirname, '..', 'tmp');
 
-  // youtubedl.exec(url,
-  //   ['-x', '--audio-format', 'mp3', '-o', '../../tmp/%(id)s.%(ext)s', '--prefer-ffmpeg'],
-  //   {},
-  //   function exec(err, output) {
-  //     'use strict';
-  //     if (err) { throw err; }
-  //     console.log(output.join('\n'));
-  //
-  //     res.send({ downloadLink: `/tmp/${filename}` });
-  // });
-  res.send({ downloadLink: `${filename}` });
+  youtubedl.exec(url,
+    ['-x', '--audio-format', 'mp3', '-o', `${file}/%(id)s.%(ext)s`, '--prefer-ffmpeg'],
+    {},
+    function exec(err, output) {
+      'use strict';
+      if (err) {
+        res.send('error please try again');
+        throw err;
+      }
+      console.log(output.join('\n'));
+
+      res.send({ downloadLink: `${filename}` });
+  });
 }
-
 
 /**
  * download sends download of mp3 file
@@ -50,8 +47,9 @@ export function download(req, res) {
       res.setHeader('Content-disposition', 'attachment; filename=' + fileId);
       res.setHeader('Content-Type', 'application/audio/mpeg3')
       let rstream = fs.createReadStream(file);
-      rstream.pipe(res);
-      fs.unlink(file);
+      rstream.pipe(res).on('finish', function() {
+        fs.unlink(file);
+      });
     } else {
       res.send('error please try again');
       res.end();
